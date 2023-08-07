eb5989af51bd254410d2fb0e6282da4caeb386ec
arjunbirsingh <110299503+arjunbirsingh@users.noreply.github.com>
2023-08-07 16:19:58 +0530

    mobile finished

--- script.js
+++ script.js
@@ -1,15 +1,35 @@
-let body=document.body;
-let toggle=0;
-function showList(){
-    let list=event.target.id;
-    let ptag=document.querySelector(`#${list} p`);
-    console.log(ptag);
-    if(toggle==0){
-    ptag.style.display="block";
-    toggle=1;
-    }
-    else{
-        ptag.style.display="none";
-        toggle=0;
-    }
+let body = document.body;
+let toggle = 0;
+function showList() {
+  let list = event.target.id;
+  let ptag = document.querySelector(`#${list} p`);
+  console.log(ptag);
+  ptag.classList.toggle("show");
+}
+let pageChangerInCont6=document.querySelector(".pageChanger");
+let guyInCont6 = document.querySelector(".cont6 #guy");
+let blablaInCont6 = document.querySelector(".cont6 #blabla");
+let linkInCont6 = document.querySelector(".cont6 #link");
+let clickable1InCont6 = document.querySelector("#clickable1");
+let clickable2InCont6 = document.querySelector("#clickable2");
+function clickable1InCont6Clicked() {
+  clickable1InCont6.classList.toggle("bgblack");
+  pageChangerInCont6.style.backgroundColor="rgb(255, 248, 4)";
+  clickable2InCont6.classList.toggle("bgblack");guyInCont6.setAttribute(
+    "src",
+    (src =
+      "https://images.prismic.io/qrates-prd/88610884-3509-4baa-aeb0-a49ed0c8300b_artist_sol.jpg?auto=compress,format")
+  );
+  console.log("clicable 1");
+}
+function clickable2InCont6Clicked() {
+  clickable2InCont6.classList.toggle("bgblack");
+  clickable1InCont6.classList.toggle("bgblack");
+  guyInCont6.setAttribute(
+    "src",
+    (src =
+      "https://images.prismic.io/qrates-prd/86e9c6a8-3732-41a8-82e3-c63d3f97b45f_artist_mason.jpg?auto=compress,format")
+  );
+  pageChangerInCont6.style.backgroundColor="orangered"
+  console.log("clickable 2");
 }
