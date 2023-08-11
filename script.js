effd0cd7ce54dab8b49c49daadbfa4da94cbb215
arjunbirsingh <110299503+arjunbirsingh@users.noreply.github.com>
2023-08-11 21:52:47 +0530

    button anim plus locomotive scroll

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+const scroll = new LocomotiveScroll({
+  el: document.querySelector("#main"),
+  smooth: true,
+  mobile: { breakpoint: 0 },
+});
+var x,y,check;
+let mousetrail=document.querySelector("#mousetrail");
+function mousetrailMover(){
+  x=event.clientX;
+  y=event.clientY;
+  mousetrail.style.top=`${y}px`;
+  mousetrail.style.left=`${x}px`;
+  // check=`${y}px`;
+}
+mousetrailMover();
+document.addEventListener("mousemove",mousetrailMover);
 let body = document.body;
 let toggle = 0;
 function showList() {
@@ -6,7 +22,7 @@ function showList() {
   console.log(ptag);
   ptag.classList.toggle("show");
 }
-let pageChangerInCont6=document.querySelector(".pageChanger");
+let pageChangerInCont6 = document.querySelector(".pageChanger");
 let guyInCont6 = document.querySelector(".cont6 #guy");
 let blablaInCont6 = document.querySelector(".cont6 #blabla");
 let linkInCont6 = document.querySelector(".cont6 #link");
@@ -14,8 +30,9 @@ let clickable1InCont6 = document.querySelector("#clickable1");
 let clickable2InCont6 = document.querySelector("#clickable2");
 function clickable1InCont6Clicked() {
   clickable1InCont6.classList.toggle("bgblack");
-  pageChangerInCont6.style.backgroundColor="rgb(255, 248, 4)";
-  clickable2InCont6.classList.toggle("bgblack");guyInCont6.setAttribute(
+  pageChangerInCont6.style.backgroundColor = "rgb(255, 248, 4)";
+  clickable2InCont6.classList.toggle("bgblack");
+  guyInCont6.setAttribute(
     "src",
     (src =
       "https://images.prismic.io/qrates-prd/88610884-3509-4baa-aeb0-a49ed0c8300b_artist_sol.jpg?auto=compress,format")
@@ -30,6 +47,6 @@ function clickable2InCont6Clicked() {
     (src =
       "https://images.prismic.io/qrates-prd/86e9c6a8-3732-41a8-82e3-c63d3f97b45f_artist_mason.jpg?auto=compress,format")
   );
-  pageChangerInCont6.style.backgroundColor="orangered"
+  pageChangerInCont6.style.backgroundColor = "orangered";
   console.log("clickable 2");
 }
